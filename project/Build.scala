a31fcfc99b778c7333b264c4c3b23d283d691f3e
Laurent Bossavit <laurent.git@bossavit.com>
2013-01-15 16:19:49 +0100

    Change name...

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "fun"
+    val appName         = "Covoiturage"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
