6ce114eabb2859635da43421e40bf149f4c936d4
Morendil <laurent.git@bossavit.com>
2013-01-16 10:00:27 +0100

    Bump version

--- project/Build.scala
+++ project/Build.scala
@@ -5,10 +5,10 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "Covoiturage"
-    val appVersion      = "1.0-SNAPSHOT"
+    val appVersion      = "1.0.1-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+      "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
